c0c0b9535fb82402b2bd23b9b5a5bd3c172acf48
Bayon Forte <bayon.forte@ASI.local>
2018-02-22 13:32:46 -0500

    add the site origin Page Builder components

---
+++ wp-content/themes/siteorigin-north/inc/contributors.php
@@ -0,0 +1,45 @@
+<?php
+return json_decode( '{
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 5705,
+		"score": 196.9932876127597,
+		"percent": 70.24242489732083
+	},
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 993,
+		"score": 74.84725673367784,
+		"percent": 26.688487072822063
+	},
+	"0f7f79a15dafbc4c1b64dfdc79c6937d": {
+		"name": "adiraomj",
+		"email": "0f7f79a15dafbc4c1b64dfdc79c6937d",
+		"loc": 346,
+		"score": 5.4159424664582945,
+		"percent": 1.931177130746896
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 49,
+		"score": 2.821510750662272,
+		"percent": 1.0060736556159733
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 8,
+		"score": 0.35168832133645683,
+		"percent": 0.12540244796209396
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 2,
+		"score": 0.0180462381366148,
+		"percent": 0.0064347955321307415
+	}
+}', true );
